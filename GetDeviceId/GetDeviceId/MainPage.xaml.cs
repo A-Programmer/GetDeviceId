8972d35f7c4f48345ad3b4e2fffbe6c4801d1a58
A-Programmer <mrsadin@gmail.com>
2018-11-02 10:12:17 +0330

    Init

---
+++ GetDeviceId/GetDeviceId/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using GetDeviceId.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GetDeviceId
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            IDevice device = DependencyService.Get<IDevice>();
+            string deviceIdentifier = device.GetIdentifier();
+            deviceIdLabel.Text = deviceIdentifier;
+        }
+    }
+}
